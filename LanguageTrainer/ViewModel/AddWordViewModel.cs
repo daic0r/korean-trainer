d1e0517e34964e5d1421f5442e3c8fc2a6ef95ed
matthias.gruen <matthias.gruen@ivv-gmbh.de>
2015-07-01 13:49:06 +0200

    Added AddWordViewModel.cs

---
+++ LanguageTrainer/ViewModel/AddWordViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LanguageTrainer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanguageTrainer.ViewModel
+{
+    public class AddWordViewModel : BaseViewModel
+    {
+
+    }
+}

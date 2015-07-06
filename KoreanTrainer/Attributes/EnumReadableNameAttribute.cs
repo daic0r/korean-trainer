86e29a69daebcaf6e1a94d447907f0476f8db56b
matthias.gruen <matthias.gruen@ivv-gmbh.de>
2015-07-06 15:25:48 +0200

    Added files

---
+++ KoreanTrainer/Attributes/EnumReadableNameAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoreanTrainer.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = true)]
+    sealed class EnumReadableNameAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        public string ReadableName { get; set; }
+
+        // This is a positional argument
+        public EnumReadableNameAttribute(string ReadableName)
+        {
+            this.ReadableName = ReadableName;
+        }
+    }
+}
